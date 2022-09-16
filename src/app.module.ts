6b33c98eb3c4b5374f24dc4217803e6b65886821
sudo.developer <erfanmashari2@gmail.com>
2022-09-16 16:59:40 +0430

    Set graphQl and apollo module in app module
    graphql and apollo module is set in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
@@ -8,6 +10,9 @@ import { dbUsername, dbPassword, dbName } from './config/secret';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
