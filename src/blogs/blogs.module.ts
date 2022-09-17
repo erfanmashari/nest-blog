7103fe196d9f45055dad230280262092f49d653c
sudo.developer <erfanmashari2@gmail.com>
2022-09-17 12:40:20 +0430

    Remove resolver for blogs module
    resolver is removed for blogs module

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -3,11 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogsController } from './blogs.controller';
 import { BlogsService } from './blogs.service';
 import { BlogEntity } from './blog.entity';
-import { BlogsResolver } from './blogs.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([BlogEntity])],
   controllers: [BlogsController],
-  providers: [BlogsService, BlogsResolver],
+  providers: [BlogsService],
 })
 export class BlogsModule {}
